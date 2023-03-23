b370f7397226468086c2de94950e347692128972
Parham Alvani <parham.alvani@gmail.com>
2023-03-23 16:45:43 +0330

    fix: add baseURL

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,10 @@
 export default defineNuxtConfig({
   ssr: false,
 
+  app: {
+    baseURL: "/taeftar",
+  },
+
   css: ["~/assets/css/circle.css", "~/assets/css/main.css"],
 
   plugins: [{ src: "~/plugins/vuex.ts" }],
