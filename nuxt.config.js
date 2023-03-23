31f3af063b0878d8a876784a1bb7816fd25c7cd2
Parham Alvani <parham.alvani@gmail.com>
2023-03-23 06:09:11 +0330

    feat: update nuxt configurtion

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,36 +1,31 @@
 export default {
-  mode: 'spa',
+  mode: "spa",
 
   loading: {
-    color: '#E39657'
+    color: "#E39657",
   },
 
   manifest: {
-    name: 'تا افطار',
-    lang: 'e',
-    version: '2.0.0',
-    short_name: 'تا افطار',
-    description: 'محاسبه میزان زمان باقی مانده تا افطار و سحر',
-    theme_color: '#E39657',
-    start_url: "/?standalone=true&utm_source=app"
+    name: "تا افطار",
+    lang: "e",
+    version: "2.0.0",
+    short_name: "تا افطار",
+    description: "محاسبه میزان زمان باقی مانده تا افطار و سحر",
+    theme_color: "#E39657",
+    start_url: "/?standalone=true&utm_source=app",
   },
-  
+
   meta: {
-    nativeUI: true
+    nativeUI: true,
   },
 
   head: {
-    title: 'تا افطار - چقد مونده تا افطار'
+    title: "تا افطار - چقد مونده تا افطار",
   },
 
-  css: [
-    '~assets/css/circle.css',
-    '~assets/css/main.css'
-  ],
+  css: ["~/assets/css/circle.css", "~/assets/css/main.css"],
 
-  modules: [
-    '@nuxtjs/axios',
-    '@nuxtjs/pwa',
-    ['@nuxtjs/google-analytics', { ua: 'UA-79535343-1' }]
-  ]
-}
+  plugins: [{ src: "~/plugins/vuex.ts" }],
+
+  modules: [],
+};
