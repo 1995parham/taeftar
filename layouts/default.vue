f03a4c8828bf0b941c30654de5d4ea530a098e12
Parham Alvani <parham.alvani@gmail.com>
2023-03-23 16:33:09 +0330

    chore: format files

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,39 +1,45 @@
 <template>
   <div class="app-container">
     <div class="header">
-
       <div class="time-selector static">
         <span>به وقت</span>
-        <span>{{$store.state.city.name}}</span>
+        <span>{{ $store.state.city.name }}</span>
       </div>
 
-      <div class="info static" v-if="$store.state.city.loc[0] && $store.state.city.loc[1]">
+      <div
+        class="info static"
+        v-if="$store.state.city.loc[0] && $store.state.city.loc[1]"
+      >
         <span>اذان مغرب : </span>
-        <span>{{$store.state.times.today['maghribo']}}</span>
-        <br/>
+        <span>{{ $store.state.times.today["maghribo"] }}</span>
+        <br />
         <span>اذان صبح : </span>
-        <span>{{$store.state.times.today['fajro']}}</span>
+        <span>{{ $store.state.times.today["fajro"] }}</span>
       </div>
     </div>
 
     <slot></slot>
 
     <div class="logo">
-      <a href="https://github.com/1995parham/taeftar" target="_blank" rel="noopener">
-        <img src="~/assets/img/fandogh.png" width="50px" height="50px" alt=""/>
+      <a
+        href="https://github.com/1995parham/taeftar"
+        target="_blank"
+        rel="noopener"
+      >
+        <img src="~/assets/img/fandogh.png" width="50px" height="50px" alt="" />
       </a>
     </div>
   </div>
 </template>
 
 <style>
-  .city-link {
-    text-decoration: none;
-    color: #E39657;
-    margin-right: 10px;
-  }
+.city-link {
+  text-decoration: none;
+  color: #e39657;
+  margin-right: 10px;
+}
 
-  .city-link.active {
-    border-bottom: 3px solid #E39657;
-  }
+.city-link.active {
+  border-bottom: 3px solid #e39657;
+}
 </style>
