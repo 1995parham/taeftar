3f916a68d1e3c7be7fc1914d6d5ab0bbb0b15c94
Parham Alvani <parham.alvani@gmail.com>
2023-03-23 12:12:22 +0330

    feat: complete city page

--- pages/city/[name].vue
+++ pages/city/[name].vue
@@ -1,7 +1,34 @@
+<script setup>
+  import Cities from '~/lib/cities'
+
+  definePageMeta({
+    validate: (route) => {
+      return Boolean(Cities[route.params.name]);
+    }
+  })
+</script>
+
+<script>
+  import {mapMutations, mapActions, mapGetters} from 'vuex'
+
+  export default {
+    mounted() {
+      console.log("Mounted");
+      this.updateCity(this.$route.params.name || '')
+      this.timer = setInterval(this.update.bind(this), 1000);
+    },
+
+    methods: {
+      ...mapMutations(['update']),
+      ...mapActions(['toggle', 'updateCity']),
+    },
+  }
+</script>
+
 <template>
   <div>
     <div class="counter static">
-      <div class="counter-circle" @click="toggle" v-if="available">
+      <div class="counter-circle" @click="toggle" v-if="$store.state.city.loc[0] && $store.state.city.loc[1]">
         <p class="counter-title">{{$store.state.r[0].val}}</p>
         <p class="counter-subtitle">{{$store.state.r[0].lbl}}</p>
         <template v-if="$store.state.r[1].val">
@@ -12,46 +39,11 @@
           <span>مونده تا </span>
           {{$store.state.to}}
         </p>
-        <img class="counter-img" src="~assets/img/eftar.png" alt=""/>
+        <img class="counter-img" src="~/assets/img/eftar.png" alt=""/>
       </div>
       <div v-else>
         <span>در حال دریافت موقعیت...</span>
       </div>
-
-      <div class="zekr">
-        <p>
-          <strong>{{$store.state.zekr.arabic}}</strong>
-        </p>
-        <p>
-          {{$store.state.zekr.persian}}
-          (<span>دعای روز</span>
-          {{$store.state.zekr.day}})
-        </p>
-      </div>
-
     </div>
-
   </div>
 </template>
-
-<script>
-  import {mapMutations, mapActions, mapGetters} from 'vuex'
-  import Cities from '../../lib/cities'
-
-  export default {
-    validate({params: {city = ''}}) {
-      return Boolean(Cities[city]);
-    },
-    async mounted() {
-      await this.updateCity(this.$route.params.name || '')
-      this.timer = setInterval(this.update.bind(this), 1000);
-    },
-    methods: {
-      ...mapMutations(['update']),
-      ...mapActions(['toggle', 'updateCity']),
-    },
-    computed: {
-      ...mapGetters(['available']),
-    }
-  }
-</script>
